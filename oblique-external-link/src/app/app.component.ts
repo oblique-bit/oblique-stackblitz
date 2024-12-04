57ca4b2f33c4f8dec763579647f153a6bd8671c8
Nina <nina.egger@bit.admin.ch>
2024-12-04 09:11:18 +0100

    feat(stackblitz/external-link): add stackblitz example
    OUI-3327

---
+++ oblique-external-link/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { ObINavigationLink } from '@oblique/oblique';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+})
+export class AppComponent {
+  topNavigation: ObINavigationLink[] = [
+    { url: 'home', label: 'Home' }
+  ];
+}
