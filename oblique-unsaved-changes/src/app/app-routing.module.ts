bfc62aa0d2945d5b1510574244c843a3c65600f7
Nina <nina.egger@bit.admin.ch>
2024-12-04 09:57:17 +0100

    feat(stackblitz/unsaved-changes): add stackblitz example
    OUI-3327

---
+++ oblique-unsaved-changes/src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { UnsavedChangesComponent } from './unsaved-changes/unsaved-changes.component';
+import { OtherPageComponent } from './other-page/other-page.component';
+import { ObUnsavedChangesGuard } from '@oblique/oblique';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'unsaved-changes', pathMatch: 'full' },
+  {
+    path: 'unsaved-changes',
+    component: UnsavedChangesComponent,
+    canDeactivate: [ObUnsavedChangesGuard],
+  },
+  { path: 'other-page', component: OtherPageComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
---
+++ oblique-unsaved-changes/src/app/other-page/other-page.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-other-page',
+  templateUrl: './other-page.component.html',
+})
+export class OtherPageComponent {}
