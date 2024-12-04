b7303b5f46271092d294a109e44ac57bf45860ea
Nina <nina.egger@bit.admin.ch>
2024-12-04 09:22:41 +0100

    feat(stackblitz/master-layout): add stackblitz example
    OUI-3327

---
+++ oblique-master-layout/src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { ObINavigationLink } from '@oblique/oblique';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+})
+export class AppComponent {
+  topNavigation: ObINavigationLink[] = [
+    { url: 'home', label: 'Home' },
+    {
+      children: [
+        {
+          label: 'Sub page',
+          url: 'sub1',
+        },
+      ],
+      label: 'With sub nav',
+      url: 'menu',
+    },
+  ];
+}
