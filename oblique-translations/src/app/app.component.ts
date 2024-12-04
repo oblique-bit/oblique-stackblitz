0ac771429b7d9281afadfdf41f35451d04eca3fe
Nina <nina.egger@bit.admin.ch>
2024-12-04 09:55:08 +0100

    feat(stackblitz/translate): add stackblitz example
    OUI-3327

---
+++ oblique-translations/src/app/app.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+import { ObINavigationLink } from '@oblique/oblique';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+})
+export class AppComponent {}
