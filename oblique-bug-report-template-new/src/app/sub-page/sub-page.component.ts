0f35fe56a8cb7d327a7cf5514c0bd2fb22ac9f8d
Nina <nina.egger@bit.admin.ch>
2025-02-12 13:23:43 +0100

    feat(stackblitz/support): update to angular 19 and oblique@13.0.0-rc.1
    OUI-3558

--- oblique-bug-report-template-new/src/app/sub-page/sub-page.component.ts
+++ oblique-bug-report-template-new/src/app/sub-page/sub-page.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-sub-page',
-  templateUrl: './sub-page.component.html',
+    selector: 'app-sub-page',
+    templateUrl: './sub-page.component.html',
+    standalone: false
 })
 export class SubPageComponent {}
