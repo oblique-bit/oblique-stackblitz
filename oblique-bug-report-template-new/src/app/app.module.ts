4d5f7e27d89fea82616a1083e7cebb48591ef857
Nina <nina.egger@bit.admin.ch>
2025-02-12 13:23:43 +0100

    feat(stackblitz/support): update to angular 19 and oblique@13.0.0-rc.1
    OUI-3558

--- oblique-bug-report-template-new/src/app/app.module.ts
+++ oblique-bug-report-template-new/src/app/app.module.ts
@@ -1,32 +1,54 @@
-import { LOCALE_ID, NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {LOCALE_ID, NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
 import {
-  ObIconModule,
   ObMasterLayoutModule,
-  multiTranslateLoader,
+  provideObliqueConfiguration
 } from '@oblique/oblique';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { registerLocaleData } from '@angular/common';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {registerLocaleData} from '@angular/common';
 import localeDECH from '@angular/common/locales/de-CH';
 import localeFRCH from '@angular/common/locales/fr-CH';
 import localeITCH from '@angular/common/locales/it-CH';
-import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
-import { TranslateModule } from '@ngx-translate/core';
-import { HomeComponent } from './home/home.component';
-import { SubPageComponent } from './sub-page/sub-page.component';
+import {provideHttpClient, withInterceptorsFromDi} from '@angular/common/http';
+import {TranslateModule} from '@ngx-translate/core';
+import {HomeComponent} from './home/home.component';
+import {SubPageComponent} from './sub-page/sub-page.component';
 
 registerLocaleData(localeDECH);
 registerLocaleData(localeFRCH);
 registerLocaleData(localeITCH);
 
-@NgModule({ declarations: [AppComponent, HomeComponent, SubPageComponent],
-    bootstrap: [AppComponent], imports: [BrowserModule,
-        AppRoutingModule,
-        ObMasterLayoutModule,
-        BrowserAnimationsModule,
-        ObIconModule.forRoot(),
-        TranslateModule.forRoot(multiTranslateLoader())], providers: [{ provide: LOCALE_ID, useValue: 'de-CH' }, provideHttpClient(withInterceptorsFromDi())] })
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    SubPageComponent
+  ],
+  bootstrap: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ObMasterLayoutModule,
+    BrowserAnimationsModule,
+    TranslateModule
+  ],
+  providers: [
+    provideObliqueConfiguration(
+      {
+        accessibilityStatement:
+        {
+          applicationName: "Replace me with the application's name",
+          applicationOperator: 'Replace me with the name and address of the federal office that exploit this application, HTML is permitted',
+          contact: {/* at least 1 email or phone number has to be provided */}
+        },
+      }),
+    {provide: LOCALE_ID, useValue: 'de-CH'},
+    provideHttpClient(withInterceptorsFromDi())
+  ]
+})
 export class AppModule {}
