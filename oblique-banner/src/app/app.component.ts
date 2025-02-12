d4367d02470be35e5b3768e9f8a99d89973ba3e2
Nina <nina.egger@bit.admin.ch>
2025-02-12 11:20:07 +0100

    feat(stackblitz/banner): update to angular 19 and oblique@13.0.0-rc.1
    OUI-3558

--- oblique-banner/src/app/app.component.ts
+++ oblique-banner/src/app/app.component.ts
@@ -2,8 +2,9 @@ import { Component } from '@angular/core';
 import { ObINavigationLink } from '@oblique/oblique';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    standalone: false
 })
 export class AppComponent {
   topNavigation: ObINavigationLink[] = [{ url: 'home', label: 'Home' }];
