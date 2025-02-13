fe96249a2ebe4f6625640c20d800bcacdab58d6d
Nina <nina.egger@bit.admin.ch>
2025-02-13 08:47:09 +0100

    feat(stackblitz/master-layout): update to angular 19 and oblique@13.0.0-rc.1
    OUI-3558

--- oblique-master-layout/src/app/app.module.ts
+++ oblique-master-layout/src/app/app.module.ts
@@ -1,22 +1,21 @@
-import { LOCALE_ID, NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {LOCALE_ID, NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
 import {
-  ObIconModule,
   ObMasterLayoutModule,
-  multiTranslateLoader,
+  provideObliqueConfiguration
 } from '@oblique/oblique';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { registerLocaleData } from '@angular/common';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {registerLocaleData} from '@angular/common';
 import localeDECH from '@angular/common/locales/de-CH';
 import localeFRCH from '@angular/common/locales/fr-CH';
 import localeITCH from '@angular/common/locales/it-CH';
-import { HttpClientModule } from '@angular/common/http';
-import { TranslateModule } from '@ngx-translate/core';
-import { HomeComponent } from './home/home.component';
-import { SubPageComponent } from './sub-page/sub-page.component';
+import {HttpClientModule} from '@angular/common/http';
+import {TranslateModule} from '@ngx-translate/core';
+import {HomeComponent} from './home/home.component';
+import {SubPageComponent} from './sub-page/sub-page.component';
 
 registerLocaleData(localeDECH);
 registerLocaleData(localeFRCH);
@@ -29,11 +28,20 @@ registerLocaleData(localeITCH);
     AppRoutingModule,
     ObMasterLayoutModule,
     BrowserAnimationsModule,
-    ObIconModule.forRoot(),
     HttpClientModule,
-    TranslateModule.forRoot(multiTranslateLoader()),
+    TranslateModule,
   ],
-  providers: [{ provide: LOCALE_ID, useValue: 'de-CH' }],
+  providers: [
+    provideObliqueConfiguration(
+      {
+        accessibilityStatement: {
+          applicationName: "Replace me with the application's name",
+          applicationOperator: 'Replace me with the name and address of the federal office that exploit this application, HTML is permitted',
+          contact: {/* at least 1 email or phone number has to be provided */}
+        },
+      }),
+    {provide: LOCALE_ID, useValue: 'de-CH'}
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
