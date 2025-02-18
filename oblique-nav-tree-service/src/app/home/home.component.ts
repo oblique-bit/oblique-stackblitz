b2d7e73ebbec44f3fde9cb7d272340a50543eff4
Nina <nina.egger@bit.admin.ch>
2025-02-18 08:31:43 +0100

    feat(stackblitz/nav-tree): add service example
    OUI-3534

---
+++ oblique-nav-tree-service/src/app/home/home.component.ts
@@ -0,0 +1,18 @@
+import { Component, inject, OnInit } from '@angular/core';
+import {ObNavTreeItemModel} from '@oblique/oblique';
+import {map, Observable, of} from 'rxjs';
+import {DataService} from './data.service';
+
+@Component({
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    standalone: false
+})
+export class HomeComponent implements OnInit{
+	items$: Observable<ObNavTreeItemModel[]>;
+	private readonly dataService = inject(DataService);
+
+	ngOnInit(): void {
+		this.items$ = this.dataService.getData().pipe(map(data => data.map(item => new ObNavTreeItemModel(item))));
+	}
+}
