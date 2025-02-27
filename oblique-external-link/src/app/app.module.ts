1c73f6f922fa4ac0b8996891ed98cbb70cac91ba
Nina <nina.egger@bit.admin.ch>
2025-02-27 13:30:24 +0100

    feat(stackblitz/external-link): update to oblique@13.0.0
    OUI-3487

--- oblique-external-link/src/app/app.module.ts
+++ oblique-external-link/src/app/app.module.ts
@@ -42,7 +42,7 @@ registerLocaleData(localeITCH);
         {
           applicationName: "Replace me with the application's name",
           applicationOperator: 'Replace me with the name and address of the federal office that exploit this application, HTML is permitted',
-          contact: {/* at least 1 email or phone number has to be provided */}
+          contact: {/* at least 1 email or phone number has to be provided */ emails: [''], phones: ['']},
         },
       }),
     {
