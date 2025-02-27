b6e218b0f5430bbca3d2f632ccaeac20f9a38980
Nina <nina.egger@bit.admin.ch>
2025-02-27 14:30:41 +0100

    feat(stackblitz/translations): update to oblique@13.0.0
    OUI-3487

--- oblique-translations/src/app/app.module.ts
+++ oblique-translations/src/app/app.module.ts
@@ -1,25 +1,24 @@
-import { inject, LOCALE_ID, NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {inject, LOCALE_ID, NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
 import {
-  ObIconModule,
   ObMasterLayoutModule,
-  multiTranslateLoader,
   ObMasterLayoutConfig,
   ObTranslateParamsModule,
+  provideObliqueConfiguration
 } from '@oblique/oblique';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { registerLocaleData } from '@angular/common';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {registerLocaleData} from '@angular/common';
 import localeDECH from '@angular/common/locales/de-CH';
 import localeFRCH from '@angular/common/locales/fr-CH';
 import localeITCH from '@angular/common/locales/it-CH';
 import localeENCH from '@angular/common/locales/en-CH';
 import localeES from '@angular/common/locales/es';
-import { HttpClientModule } from '@angular/common/http';
-import { TranslateModule } from '@ngx-translate/core';
-import { TranslationsComponent } from './translations/translations.component';
+import {HttpClientModule} from '@angular/common/http';
+import {TranslateModule} from '@ngx-translate/core';
+import {TranslationsComponent} from './translations/translations.component';
 
 registerLocaleData(localeDECH);
 registerLocaleData(localeENCH);
@@ -34,12 +33,22 @@ registerLocaleData(localeITCH);
     AppRoutingModule,
     ObMasterLayoutModule,
     BrowserAnimationsModule,
-    ObIconModule.forRoot(),
     ObTranslateParamsModule,
     HttpClientModule,
-    TranslateModule.forRoot(multiTranslateLoader()),
+    TranslateModule,
   ],
-  providers: [{ provide: LOCALE_ID, useValue: 'de-CH' }],
+  providers: [
+    provideObliqueConfiguration(
+      {
+        accessibilityStatement:
+        {
+          applicationName: "Replace me with the application's name",
+          applicationOperator: 'Replace me with the name and address of the federal office that exploit this application, HTML is permitted',
+          contact: {/* at least 1 email or phone number has to be provided */ emails: [''], phones: ['']},
+        },
+      }),
+    { provide: LOCALE_ID, useValue: 'de-CH' }
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {
--- oblique-translations/src/app/translations/translations.component.ts
+++ oblique-translations/src/app/translations/translations.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-translations',
-  templateUrl: './translations.component.html',
-  styleUrls: ['./translations.component.scss'],
+    selector: 'app-translations',
+    templateUrl: './translations.component.html',
+    styleUrls: ['./translations.component.scss'],
+    standalone: false
 })
 export class TranslationsComponent {
   readonly failedFiles: string = [
