73eabebc6de0b8051aaa47517b8af94c84db76b4
René Giller <rene.giller@gmail.com>
2025-03-17 19:18:12 +0100

    refactor(toolchain): simplify dependencies update script
    OUI-3652

--- scripts/dependencies-update.ts
+++ scripts/dependencies-update.ts
@@ -6,24 +6,19 @@ import {Log} from './shared/log';
 class DependenciesUpdate extends StaticScript {
 
 	static perform(): void {
-		const rootDirectory = Files.buildOSSafePath(__dirname, '/../');
 		const excludedProjects = ['oblique-language-change-in-master-layout'];
 
-		const allProjectPaths = Files.readDirectory(rootDirectory)
-			.filter(project => Files.exists(Files.buildOSSafePath(rootDirectory, project, 'package.json')))
-			.filter(foundProject => excludedProjects.every(
-				excludedProject => Files.buildOSSafePath(rootDirectory, excludedProject, 'package.json') !== Files.buildOSSafePath(rootDirectory, foundProject, 'package.json'))
-			)
-			.map(filteredProject => Files.buildOSSafePath(rootDirectory, filteredProject));
-
-		allProjectPaths.forEach(path => {
-			Log.start('Update dependencies');
-			DependenciesUpdate.execute(`npm update --prefix ${path} --save --audit false`);
-			DependenciesUpdate.execute(`npm audit fix --prefix ${path} --audit-level=none`);
-			DependenciesUpdate.execute(`npm dedupe --prefix ${path} --audit false`);
-			DependenciesUpdate.execute(`npm prune --prefix ${path} --audit false`);
-			Log.success();
-		})
+		Files.readDirectory('.')
+			.filter(project => Files.exists(`${project}/package.json`))
+			.filter(project => !excludedProjects.includes(project))
+			.forEach(project => {
+				Log.start('Update dependencies');
+				DependenciesUpdate.execute(`npm update --prefix ./${project} --save --audit false`);
+				DependenciesUpdate.execute(`npm audit fix --prefix ./${project} --audit-level=none`);
+				DependenciesUpdate.execute(`npm dedupe --prefix ./${project} --audit false`);
+				DependenciesUpdate.execute(`npm prune --prefix ./${project} --audit false`);
+				Log.success();
+			});
 	}
 
 	private static execute(command: string): void {
