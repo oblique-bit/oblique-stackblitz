0c71a80a8a5eadcd15a17ae2352160099e79e7ca
René Giller <rene.giller@gmail.com>
2025-03-17 19:26:52 +0100

    feat(toolchain): make a commit per folder
    OUI-3652

--- scripts/dependencies-update.ts
+++ scripts/dependencies-update.ts
@@ -1,6 +1,6 @@
 import {StaticScript} from './shared/static-script';
 import {Files} from './shared/files';
-import {executeCommandWithLog} from './shared/utils';
+import {executeCommandWithLog, getResultFromCommand} from './shared/utils';
 import {Log} from './shared/log';
 
 class DependenciesUpdate extends StaticScript {
@@ -12,17 +12,22 @@ class DependenciesUpdate extends StaticScript {
 			.filter(project => Files.exists(`${project}/package.json`))
 			.filter(project => !excludedProjects.includes(project))
 			.forEach(project => {
-				Log.start('Update dependencies');
+				Log.start(`Update dependencies of ${project}`);
 				DependenciesUpdate.execute(`npm update --prefix ./${project} --save --audit false`);
 				DependenciesUpdate.execute(`npm audit fix --prefix ./${project} --audit-level=none`);
 				DependenciesUpdate.execute(`npm dedupe --prefix ./${project} --audit false`);
 				DependenciesUpdate.execute(`npm prune --prefix ./${project} --audit false`);
+				executeCommandWithLog(`git commit -am "feat(stackblitz/${project}): update to oblique@${DependenciesUpdate.getVersion()}"`, 'Execute');
 				Log.success();
 			});
 	}
 
 	private static execute(command: string): void {
 		executeCommandWithLog(`${command} --fund false`, `Execute`);
+	}
+
+	private static getVersion(): string {
+		return /(?<=latest:\s*)\d+\.\d+\.\d+(?:-(?:alpha|beta|rc)\.\d+)?/.exec(getResultFromCommand('npm dist-tag @oblique/oblique'))[0];
 	}
 }
 
