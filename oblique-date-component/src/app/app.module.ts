55f4263ce33cd0e915f4f110de696179647057e4
Nina Egger <nina.egger@bit.admin.ch>
2025-06-13 13:03:11 +0200

    feat(stackblitz/oblique-date-component-example): add ObDateComponentExample
    OUI_3819

---
+++ oblique-date-component/src/app/app.module.ts
@@ -0,0 +1,58 @@
+import {inject, LOCALE_ID, NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {
+  ObMasterLayoutModule,
+  ObMasterLayoutConfig,
+  ObLanguageModule,
+  provideObliqueConfiguration
+} from '@oblique/oblique';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {registerLocaleData} from '@angular/common';
+import localeDECH from '@angular/common/locales/de-CH';
+import localeFRCH from '@angular/common/locales/fr-CH';
+import localeITCH from '@angular/common/locales/it-CH';
+import localeEN from '@angular/common/locales/en';
+import {HttpClientModule} from '@angular/common/http';
+import {TranslateModule} from '@ngx-translate/core';
+import {HomeComponent} from './home/home.component';
+
+registerLocaleData(localeDECH);
+registerLocaleData(localeFRCH);
+registerLocaleData(localeITCH);
+registerLocaleData(localeEN);
+
+@NgModule({
+  declarations: [AppComponent, HomeComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ObMasterLayoutModule,
+    ObLanguageModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    TranslateModule,
+  ],
+  providers: [
+    provideObliqueConfiguration(
+      {
+        accessibilityStatement:
+        {
+          applicationName: "Oblique ObDateComponent example",
+          applicationOperator: 'Bundesamt für Informatik und Telekommunikation BIT<br>Campus Meielen<br>Eichenweg 3<br>CH-3003 Bern',
+          contact: {emails: ['oblique@bit.admin.ch']}
+        },
+      }),
+    {provide: LOCALE_ID, useValue: 'de-CH'}
+  ],
+  bootstrap: [AppComponent],
+})
+export class AppModule {
+  private readonly config = inject(ObMasterLayoutConfig);
+
+  constructor() {
+    this.config.locale.locales = ['de-CH', 'en-US'];
+  }
+}
