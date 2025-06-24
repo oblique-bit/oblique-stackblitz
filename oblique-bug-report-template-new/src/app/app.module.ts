2f8100128460c70d01d275a186c5da7e62ce7626
Nina Egger <nina.egger@bit.admin.ch>
2025-06-24 08:28:34 +0200

    feat(stackblitz/oblique-bug-report-template-new): provide relevant accessibility information
    OUI-3862

--- oblique-bug-report-template-new/src/app/app.module.ts
+++ oblique-bug-report-template-new/src/app/app.module.ts
@@ -42,9 +42,9 @@ registerLocaleData(localeITCH);
       {
         accessibilityStatement:
         {
-          applicationName: "Replace me with the application's name",
-          applicationOperator: 'Replace me with the name and address of the federal office that exploit this application, HTML is permitted',
-          contact: {/* at least 1 email or phone number has to be provided */ emails: [''], phones: ['']},
+          applicationName: "Oblique: Bug Report Template",
+          applicationOperator: 'Federal Office of Information Technology, Systems and Telecommunication FOITT<br>Meielen Campus<br>Eichenweg 3<br>CH-3003 Bern',
+          contact: {emails: ['oblique@bit.admin.ch']},
         },
       }),
     {provide: LOCALE_ID, useValue: 'de-CH'},
