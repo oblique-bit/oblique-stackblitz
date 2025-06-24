73a0cf8602f98dbcd458c08c86ae96db0c03076d
Nina Egger <nina.egger@bit.admin.ch>
2025-06-24 09:12:45 +0200

    feat(stackblitz/oblique-off-canvas): provide relevant accessibility information
    OUI-3862

--- oblique-off-canvas/src/app/app.module.ts
+++ oblique-off-canvas/src/app/app.module.ts
@@ -31,9 +31,9 @@ import {HttpClientModule} from '@angular/common/http';
       {
         accessibilityStatement:
         {
-          applicationName: "Replace me with the application's name",
-          applicationOperator: 'Replace me with the name and address of the federal office that exploit this application, HTML is permitted',
-          contact: {/* at least 1 email or phone number has to be provided */ emails: [''], phones: ['']},
+          applicationName: "Oblique Off Canvas example",
+          applicationOperator: 'Federal Office of Information Technology, Systems and Telecommunication FOITT<br>Meielen Campus<br>Eichenweg 3<br>CH-3003 Bern',
+          contact: {emails: ['oblique@bit.admin.ch']},
         },
       }),
   ],
