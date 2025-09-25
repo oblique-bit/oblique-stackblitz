9a2a1d1f26dd743583014c1f0a33f2dbaddfac29
Nina Egger <nina.egger@bit.admin.ch>
2025-09-25 13:16:44 +0200

    feat(stackblitz/oblique-column-layout): update to oblique@14.0.0
    OUI-4073

--- oblique-column-layout/src/app/app.module.ts
+++ oblique-column-layout/src/app/app.module.ts
@@ -6,17 +6,17 @@ import {AppComponent} from './app.component';
 import {
   ObMasterLayoutModule,
   ObColumnLayoutModule,
-  provideObliqueConfiguration
+  provideObliqueConfiguration,
+  ObMasterLayoutConfig
 } from '@oblique/oblique';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {registerLocaleData} from '@angular/common';
 import localeDECH from '@angular/common/locales/de-CH';
 import localeFRCH from '@angular/common/locales/fr-CH';
 import localeITCH from '@angular/common/locales/it-CH';
-import {HttpClientModule} from '@angular/common/http';
+import {provideHttpClient, withInterceptorsFromDi} from '@angular/common/http';
 import {TranslateModule} from '@ngx-translate/core';
 import {HomeComponent} from './home/home.component';
-import {ObMasterLayoutConfig} from '@oblique/oblique';
 import {inject} from '@angular/core';
 
 registerLocaleData(localeDECH);
@@ -34,24 +34,26 @@ registerLocaleData(localeITCH);
     ObMasterLayoutModule,
     ObColumnLayoutModule,
     BrowserAnimationsModule,
-    HttpClientModule,
     TranslateModule,
   ],
   providers: [
     provideObliqueConfiguration(
       {
         accessibilityStatement: {
+          createdOn: new Date('2025-09-25'),
+          conformity: 'none',
           applicationName: "Oblique Column Layout Example",
           applicationOperator: 'Federal Office of Information Technology, Systems and Telecommunication FOITT<br>Meielen Campus<br>Eichenweg 3<br>CH-3003 Bern',
-          contact: {emails: ['oblique@bit.admin.ch']},
+          contact: [{email: 'oblique@bit.admin.ch'}],
         },
       }),
-    {provide: LOCALE_ID, useValue: 'de-CH'}
+    {provide: LOCALE_ID, useValue: 'de-CH'},
+    provideHttpClient(withInterceptorsFromDi())
   ],
   bootstrap: [AppComponent],
 })
 export class AppModule {
-  
+
   constructor() {
     const masterLayoutConfig = inject(ObMasterLayoutConfig)
     masterLayoutConfig.homePageRoute = "/oblique-column-layout";
