f54f66d54fd06d6d3927dd0b3d2192d42acc8b65
Nina Egger <nina.egger@bit.admin.ch>
2025-09-25 11:18:21 +0200

    feat(stackblitz/oblique-bug-report-template-new): update to oblique@14.0.0
    OUI-4073

--- oblique-bug-report-template-new/src/app/app.module.ts
+++ oblique-bug-report-template-new/src/app/app.module.ts
@@ -5,7 +5,8 @@ import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {
   ObMasterLayoutModule,
-  provideObliqueConfiguration
+  provideObliqueConfiguration,
+  ObMasterLayoutConfig
 } from '@oblique/oblique';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {registerLocaleData} from '@angular/common';
@@ -16,7 +17,6 @@ import {provideHttpClient, withInterceptorsFromDi} from '@angular/common/http';
 import {TranslateModule} from '@ngx-translate/core';
 import {HomeComponent} from './home/home.component';
 import {SubPageComponent} from './sub-page/sub-page.component';
-import {ObMasterLayoutConfig} from '@oblique/oblique';
 
 registerLocaleData(localeDECH);
 registerLocaleData(localeFRCH);
@@ -43,9 +43,11 @@ registerLocaleData(localeITCH);
       {
         accessibilityStatement:
         {
+				createdOn: new Date('2025-09-25'),
+				conformity: 'none',
           applicationName: "Oblique: Bug Report Template",
           applicationOperator: 'Federal Office of Information Technology, Systems and Telecommunication FOITT<br>Meielen Campus<br>Eichenweg 3<br>CH-3003 Bern',
-          contact: {emails: ['oblique@bit.admin.ch']},
+          contact:[{email: 'oblique@bit.admin.ch'}],
         },
       }),
     {provide: LOCALE_ID, useValue: 'de-CH'},
