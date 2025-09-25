7ff120f8e28c84b278c0869d93ad2f9d63841c7d
Nina Egger <nina.egger@bit.admin.ch>
2025-09-25 10:51:09 +0200

    feat(stackblitz/oblique-breadcrumb): update to oblique@14.0.0
    OUI-4073

--- oblique-breadcrumb/src/app/app.module.ts
+++ oblique-breadcrumb/src/app/app.module.ts
@@ -14,10 +14,10 @@ import {registerLocaleData} from '@angular/common';
 import localeDECH from '@angular/common/locales/de-CH';
 import localeFRCH from '@angular/common/locales/fr-CH';
 import localeITCH from '@angular/common/locales/it-CH';
-import {HttpClientModule} from '@angular/common/http';
 import {TranslateModule} from '@ngx-translate/core';
 import {BreadcrumbSampleComponent} from './breadcrumb-sample/breadcrumb-sample.component';
 import {ObMasterLayoutConfig} from '@oblique/oblique';
+import {provideHttpClient, withInterceptorsFromDi} from '@angular/common/http';
 
 registerLocaleData(localeDECH);
 registerLocaleData(localeFRCH);
@@ -31,20 +31,22 @@ registerLocaleData(localeITCH);
     ObMasterLayoutModule,
     ObBreadcrumbModule,
     BrowserAnimationsModule,
-    HttpClientModule,
     TranslateModule,
   ],
   providers: [
     provideObliqueConfiguration(
       {
         accessibilityStatement: {
+				createdOn: new Date('2025-09-25'),
+				conformity: 'none',
           applicationName: "Oblique Breadcrumb example",
           applicationOperator: 'Federal Office of Information Technology, Systems and Telecommunication FOITT<br>Meielen Campus<br>Eichenweg 3<br>CH-3003 Bern',
-          contact: {emails: ['oblique@bit.admin.ch']},
+          contact:[{email: 'oblique@bit.admin.ch'}],
         },
       }),
     {provide: LOCALE_ID, useValue: 'de-CH'},
     {provide: OB_BANNER, useValue: {text: 'DEMO'}},
+    provideHttpClient(withInterceptorsFromDi()),
   ],
   bootstrap: [AppComponent],
 })
