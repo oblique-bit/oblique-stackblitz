587cb1e73ed52789187fe2ab3a0306fae07051fc
Nina Egger <nina.egger@bit.admin.ch>
2025-09-26 09:07:02 +0200

    feat(stackblitz/oblique-oblique-unknown-route-example): update to oblique@14.0.0
    OUI-4073

--- oblique-oblique-unknown-route-example/src/app/app.module.ts
+++ oblique-oblique-unknown-route-example/src/app/app.module.ts
@@ -6,17 +6,17 @@ import {AppComponent} from './app.component';
 import {
   ObMasterLayoutModule,
   ObUnknownRouteModule,
-  provideObliqueConfiguration
+  provideObliqueConfiguration,
+  ObMasterLayoutConfig
 } from '@oblique/oblique';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {registerLocaleData} from '@angular/common';
 import localeDECH from '@angular/common/locales/de-CH';
 import localeFRCH from '@angular/common/locales/fr-CH';
 import localeITCH from '@angular/common/locales/it-CH';
-import {HttpClientModule} from '@angular/common/http';
+import {provideHttpClient, withInterceptorsFromDi} from '@angular/common/http';
 import {TranslateModule} from '@ngx-translate/core';
 import {HomeComponent} from './home/home.component';
-import {ObMasterLayoutConfig} from '@oblique/oblique';
 import {inject} from '@angular/core';
 
 registerLocaleData(localeDECH);
@@ -31,7 +31,6 @@ registerLocaleData(localeITCH);
     ObMasterLayoutModule,
     ObUnknownRouteModule,
     BrowserAnimationsModule,
-    HttpClientModule,
     TranslateModule,
   ],
   providers: [
@@ -39,18 +38,21 @@ registerLocaleData(localeITCH);
       {
         accessibilityStatement:
         {
+          createdOn: new Date('2025-09-26'),
+          conformity: 'none',
           applicationName: "Oblique Unknown Route example",
           applicationOperator: 'Federal Office of Information Technology, Systems and Telecommunication FOITT<br>Meielen Campus<br>Eichenweg 3<br>CH-3003 Bern',
-          contact: {emails: ['oblique@bit.admin.ch']},
+          contact: [{email: 'oblique@bit.admin.ch'}],
         },
       }),
-    {provide: LOCALE_ID, useValue: 'de-CH'}
+    {provide: LOCALE_ID, useValue: 'de-CH'},
+    provideHttpClient(withInterceptorsFromDi())
   ],
   bootstrap: [AppComponent],
 })
 export class AppModule {
   constructor() {
     const masterLayoutConfig = inject(ObMasterLayoutConfig);
-    masterLayoutConfig.homePageRoute = "/oblique-oblique-unknown-route-example";
+    masterLayoutConfig.homePageRoute = "/oblique-unknown-route-example";
   };
 }
