2489e3b4bb5cf2299a6df3fbe70135e9f9452d1f
Nina Egger <nina.egger@bit.admin.ch>
2025-09-26 08:21:42 +0200

    feat(stackblitz/oblique-nav-tree-filter): update to oblique@14.0.0
    OUI-4073

--- oblique-nav-tree-filter/src/app/app.module.ts
+++ oblique-nav-tree-filter/src/app/app.module.ts
@@ -6,17 +6,17 @@ import {AppComponent} from './app.component';
 import {
   ObMasterLayoutModule,
   ObNavTreeModule,
-  provideObliqueConfiguration
+  provideObliqueConfiguration,
+  ObMasterLayoutConfig
 } from '@oblique/oblique';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {registerLocaleData} from '@angular/common';
 import localeDECH from '@angular/common/locales/de-CH';
 import localeFRCH from '@angular/common/locales/fr-CH';
 import localeITCH from '@angular/common/locales/it-CH';
-import {HttpClientModule} from '@angular/common/http';
+import {provideHttpClient, withInterceptorsFromDi} from '@angular/common/http';
 import {TranslateModule} from '@ngx-translate/core';
 import {HomeComponent} from './home/home.component';
-import {ObMasterLayoutConfig} from '@oblique/oblique';
 import {inject} from '@angular/core';
 
 registerLocaleData(localeDECH);
@@ -34,19 +34,21 @@ registerLocaleData(localeITCH);
     ObNavTreeModule,
     ObMasterLayoutModule,
     BrowserAnimationsModule,
-    HttpClientModule,
     TranslateModule,
   ],
   providers: [
     provideObliqueConfiguration({
       accessibilityStatement:
       {
+        createdOn: new Date('2025-09-26'),
+        conformity: 'none',
         applicationName: "Oblique Nav Tree Filter example",
         applicationOperator: 'Federal Office of Information Technology, Systems and Telecommunication FOITT<br>Meielen Campus<br>Eichenweg 3<br>CH-3003 Bern',
-        contact: {emails: ['oblique@bit.admin.ch']},
+        contact: [{email: 'oblique@bit.admin.ch'}],
       },
     }),
-    {provide: LOCALE_ID, useValue: 'de-CH'}
+    {provide: LOCALE_ID, useValue: 'de-CH'},
+    provideHttpClient(withInterceptorsFromDi())
   ],
   bootstrap: [AppComponent],
 })
