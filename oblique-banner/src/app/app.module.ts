24faf8eec18e6d1e0b6d436900cd1298bfac2782
Giller René BIT <rene.giller@bit.admin.ch>
2026-02-02 16:21:42 +0100

    feat(oblique-banner): update to oblique@15.0.0

--- oblique-banner/src/app/app.module.ts
+++ oblique-banner/src/app/app.module.ts
@@ -9,7 +9,6 @@ import {
   provideObliqueConfiguration,
   ObMasterLayoutConfig
 } from '@oblique/oblique';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {registerLocaleData} from '@angular/common';
 import localeDECH from '@angular/common/locales/de-CH';
 import localeFRCH from '@angular/common/locales/fr-CH';
@@ -28,7 +27,7 @@ registerLocaleData(localeITCH);
     BrowserModule,
     AppRoutingModule,
     ObMasterLayoutModule,
-    BrowserAnimationsModule,
+    
     TranslateModule,
   ],
   providers: [
