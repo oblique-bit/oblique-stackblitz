e80d3df0a954a7dcf2fc5ee5e6809fe0205cd7b6
Nina Egger <nina.egger@bit.admin.ch>
2026-09-10 09:11:00 +0200

    feat(oblique-banner): update to oblique@16.0.0-rc.1

--- oblique-banner/src/app/app-routing.module.ts
+++ oblique-banner/src/app/app-routing.module.ts
@@ -1,10 +1,10 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { HomeComponent } from "./home/home.component";
 
 const routes: Routes = [
-  { path: '', redirectTo: 'oblique-banner', pathMatch: 'full' },
-  { path: 'oblique-banner', component: HomeComponent },
+  { path: "", redirectTo: "oblique-banner", pathMatch: "full" },
+  { path: "oblique-banner", component: HomeComponent },
 ];
 
 @NgModule({
