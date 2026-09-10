8c29663f63d804d1d84f50a4a37e9925271a1a09
Nina Egger <nina.egger@bit.admin.ch>
2026-09-10 15:43:05 +0200

    feat(oblique-nav-tree-default): update to oblique@16.0.0-rc.1

--- oblique-nav-tree-default/src/app/app-routing.module.ts
+++ oblique-nav-tree-default/src/app/app-routing.module.ts
@@ -1,10 +1,10 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { HomeComponent } from "./home/home.component";
 
 const routes: Routes = [
-  { path: '', redirectTo: 'oblique-nav-tree-default', pathMatch: 'full' },
-  { path: 'oblique-nav-tree-default', component: HomeComponent }
+  { path: "", redirectTo: "oblique-nav-tree-default", pathMatch: "full" },
+  { path: "oblique-nav-tree-default", component: HomeComponent },
 ];
 
 @NgModule({
--- oblique-nav-tree-default/src/app/app.module.ts
+++ oblique-nav-tree-default/src/app/app.module.ts
@@ -1,53 +1,51 @@
-import {LOCALE_ID, NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
+import {LOCALE_ID, NgModule} from "@angular/core";
+import {BrowserModule} from "@angular/platform-browser";
 
-import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './app.component';
+import {AppRoutingModule} from "./app-routing.module";
+import {AppComponent} from "./app.component";
 import {
   ObMasterLayoutModule,
   ObNavTreeModule,
   provideObliqueConfiguration,
-  ObMasterLayoutConfig
-} from '@oblique/oblique';
-import {registerLocaleData} from '@angular/common';
-import localeDECH from '@angular/common/locales/de-CH';
-import localeFRCH from '@angular/common/locales/fr-CH';
-import localeITCH from '@angular/common/locales/it-CH';
-import {provideHttpClient, withInterceptorsFromDi} from '@angular/common/http';
-import {TranslateModule} from '@ngx-translate/core';
-import {HomeComponent} from './home/home.component';
-import {inject} from '@angular/core';
+  ObMasterLayoutConfig,
+} from "@oblique/oblique";
+import {registerLocaleData} from "@angular/common";
+import localeDECH from "@angular/common/locales/de-CH";
+import localeFRCH from "@angular/common/locales/fr-CH";
+import localeITCH from "@angular/common/locales/it-CH";
+import {
+  provideHttpClient,
+  withInterceptorsFromDi,
+  withXhr,
+} from "@angular/common/http";
+import {HomeComponent} from "./home/home.component";
+import {inject} from "@angular/core";
 
 registerLocaleData(localeDECH);
 registerLocaleData(localeFRCH);
 registerLocaleData(localeITCH);
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent
-  ],
+  declarations: [AppComponent, HomeComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     ObNavTreeModule,
-    ObMasterLayoutModule,
-    
-    TranslateModule,
+    ObMasterLayoutModule
   ],
   providers: [
     provideObliqueConfiguration({
-      accessibilityStatement:
-      {
-        createdOn: new Date('2025-09-25'),
-        conformity: 'none',
+      accessibilityStatement: {
+        createdOn: new Date("2025-09-25"),
+        conformity: "none",
         applicationName: "Oblique Nav Tree Default example",
-        applicationOperator: 'Federal Office of Information Technology, Systems and Telecommunication FOITT<br>Meielen Campus<br>Eichenweg 3<br>CH-3003 Bern',
-        contact: [{email: 'oblique@bit.admin.ch'}],
+        applicationOperator:
+          "Federal Office of Information Technology, Systems and Telecommunication FOITT<br>Meielen Campus<br>Eichenweg 3<br>CH-3003 Bern",
+        contact: [{email: "oblique@bit.admin.ch"}],
       },
     }),
-    {provide: LOCALE_ID, useValue: 'de-CH'},
-    provideHttpClient(withInterceptorsFromDi())
+    {provide: LOCALE_ID, useValue: "de-CH"},
+    provideHttpClient(withXhr(), withInterceptorsFromDi()),
   ],
   bootstrap: [AppComponent],
 })
@@ -55,5 +53,5 @@ export class AppModule {
   constructor() {
     const masterLayoutConfig = inject(ObMasterLayoutConfig);
     masterLayoutConfig.homePageRoute = "/oblique-nav-tree-default";
-  };
+  }
 }
