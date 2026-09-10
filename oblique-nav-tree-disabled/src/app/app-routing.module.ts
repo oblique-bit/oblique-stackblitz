ef87ed8a77999606d3aad725b0a1e786b0315c2a
Nina Egger <nina.egger@bit.admin.ch>
2026-09-10 15:58:59 +0200

    feat(oblique-nav-tree-disabled): update to oblique@16.0.0-rc.1

--- oblique-nav-tree-disabled/src/app/app-routing.module.ts
+++ oblique-nav-tree-disabled/src/app/app-routing.module.ts
@@ -1,10 +1,10 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { HomeComponent } from "./home/home.component";
 
 const routes: Routes = [
-  { path: '', redirectTo: 'oblique-nav-tree-disabled', pathMatch: 'full' },
-  { path: 'oblique-nav-tree-disabled', component: HomeComponent }
+  { path: "", redirectTo: "oblique-nav-tree-disabled", pathMatch: "full" },
+  { path: "oblique-nav-tree-disabled", component: HomeComponent },
 ];
 
 @NgModule({
