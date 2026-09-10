0940adfcb4e25dc21a0f816ded9d7602c375a6f4
Nina Egger <nina.egger@bit.admin.ch>
2026-09-10 12:51:01 +0200

    feat(oblique-date-component): update to oblique@16.0.0-rc.1

--- oblique-date-component/src/app/app.module.ts
+++ oblique-date-component/src/app/app.module.ts
@@ -1,22 +1,24 @@
-import {inject, LOCALE_ID, NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
-
-import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './app.component';
+import {inject, LOCALE_ID, NgModule} from "@angular/core";
+import {BrowserModule} from "@angular/platform-browser";
+import {AppRoutingModule} from "./app-routing.module";
+import {AppComponent} from "./app.component";
 import {
   ObMasterLayoutModule,
   ObMasterLayoutConfig,
   ObLanguageModule,
-  provideObliqueConfiguration
-} from '@oblique/oblique';
-import {registerLocaleData} from '@angular/common';
-import localeDECH from '@angular/common/locales/de-CH';
-import localeFRCH from '@angular/common/locales/fr-CH';
-import localeITCH from '@angular/common/locales/it-CH';
-import localeEN from '@angular/common/locales/en';
-import {provideHttpClient, withInterceptorsFromDi} from '@angular/common/http';
-import {TranslateModule} from '@ngx-translate/core';
-import {HomeComponent} from './home/home.component';
+  provideObliqueConfiguration,
+} from "@oblique/oblique";
+import {registerLocaleData} from "@angular/common";
+import localeDECH from "@angular/common/locales/de-CH";
+import localeFRCH from "@angular/common/locales/fr-CH";
+import localeITCH from "@angular/common/locales/it-CH";
+import localeEN from "@angular/common/locales/en";
+import {
+  provideHttpClient,
+  withInterceptorsFromDi,
+  withXhr,
+} from "@angular/common/http";
+import {HomeComponent} from "./home/home.component";
 
 registerLocaleData(localeDECH);
 registerLocaleData(localeFRCH);
@@ -30,33 +32,34 @@ registerLocaleData(localeEN);
     AppRoutingModule,
     ObMasterLayoutModule,
     ObLanguageModule,
-    
-    TranslateModule,
   ],
   providers: [
-    provideObliqueConfiguration(
-      {
-        accessibilityStatement:
-        {
-          createdOn: new Date('2025-09-25'),
-          conformity: 'none',
-          applicationName: "Oblique ObDateComponent example",
-          applicationOperator: 'Bundesamt für Informatik und Telekommunikation BIT<br>Campus Meielen<br>Eichenweg 3<br>CH-3003 Bern',
-          contact: [{email: 'oblique@bit.admin.ch'}]
-        },
-      }),
-    {provide: LOCALE_ID, useValue: 'de-CH'},
-    provideHttpClient(withInterceptorsFromDi())
+    provideObliqueConfiguration({
+      accessibilityStatement: {
+        createdOn: new Date("2025-09-25"),
+        conformity: "none",
+        applicationName: "Oblique ObDateComponent example",
+        applicationOperator:
+          "Bundesamt für Informatik und Telekommunikation BIT<br>Campus Meielen<br>Eichenweg 3<br>CH-3003 Bern",
+        contact: [{email: "oblique@bit.admin.ch"}],
+      },
+      translate: {
+        locales: {
+          locales: ['de-CH', 'en-US'],
+          defaultLanguage: 'de',
+          disabled: false
+        }
+      }
+    }),
+    {provide: LOCALE_ID, useValue: "de-CH"},
+    provideHttpClient(withXhr(), withInterceptorsFromDi()),
   ],
   bootstrap: [AppComponent],
 })
 export class AppModule {
-  private readonly config = inject(ObMasterLayoutConfig);
 
   constructor() {
-    this.config.locale.locales = ['de-CH', 'en-US'];
-
-    const masterLayoutConfig = inject(ObMasterLayoutConfig)
+    const masterLayoutConfig = inject(ObMasterLayoutConfig);
     masterLayoutConfig.homePageRoute = "/oblique-date-component";
-  };
+  }
 }
