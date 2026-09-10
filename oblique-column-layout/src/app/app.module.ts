0560ec37fbcea230a62a7ebbd39c37c19068fa02
Nina Egger <nina.egger@bit.admin.ch>
2026-09-10 12:09:43 +0200

    feat(oblique-column-layout): update to oblique@16.0.0-rc.1

--- oblique-column-layout/src/app/app.module.ts
+++ oblique-column-layout/src/app/app.module.ts
@@ -1,60 +1,57 @@
-import {LOCALE_ID, NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
+import {LOCALE_ID, NgModule} from "@angular/core";
+import {BrowserModule} from "@angular/platform-browser";
 
-import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './app.component';
+import {AppRoutingModule} from "./app-routing.module";
+import {AppComponent} from "./app.component";
 import {
   ObMasterLayoutModule,
   ObColumnLayoutModule,
   provideObliqueConfiguration,
-  ObMasterLayoutConfig
-} from '@oblique/oblique';
-import {registerLocaleData} from '@angular/common';
-import localeDECH from '@angular/common/locales/de-CH';
-import localeFRCH from '@angular/common/locales/fr-CH';
-import localeITCH from '@angular/common/locales/it-CH';
-import {provideHttpClient, withInterceptorsFromDi} from '@angular/common/http';
-import {TranslateModule} from '@ngx-translate/core';
-import {HomeComponent} from './home/home.component';
-import {inject} from '@angular/core';
+  ObMasterLayoutConfig,
+} from "@oblique/oblique";
+import {registerLocaleData} from "@angular/common";
+import localeDECH from "@angular/common/locales/de-CH";
+import localeFRCH from "@angular/common/locales/fr-CH";
+import localeITCH from "@angular/common/locales/it-CH";
+import {
+  provideHttpClient,
+  withInterceptorsFromDi,
+  withXhr,
+} from "@angular/common/http";
+import {HomeComponent} from "./home/home.component";
+import {inject} from "@angular/core";
 
 registerLocaleData(localeDECH);
 registerLocaleData(localeFRCH);
 registerLocaleData(localeITCH);
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent
-  ],
+  declarations: [AppComponent, HomeComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     ObMasterLayoutModule,
     ObColumnLayoutModule,
-    
-    TranslateModule,
   ],
   providers: [
-    provideObliqueConfiguration(
-      {
-        accessibilityStatement: {
-          createdOn: new Date('2025-09-25'),
-          conformity: 'none',
-          applicationName: "Oblique Column Layout Example",
-          applicationOperator: 'Federal Office of Information Technology, Systems and Telecommunication FOITT<br>Meielen Campus<br>Eichenweg 3<br>CH-3003 Bern',
-          contact: [{email: 'oblique@bit.admin.ch'}],
-        },
-      }),
-    {provide: LOCALE_ID, useValue: 'de-CH'},
-    provideHttpClient(withInterceptorsFromDi())
+    provideObliqueConfiguration({
+      accessibilityStatement: {
+        createdOn: new Date("2025-09-25"),
+        conformity: "none",
+        applicationName: "Oblique Column Layout Example",
+        applicationOperator:
+          "Federal Office of Information Technology, Systems and Telecommunication FOITT<br>Meielen Campus<br>Eichenweg 3<br>CH-3003 Bern",
+        contact: [{email: "oblique@bit.admin.ch"}],
+      },
+    }),
+    {provide: LOCALE_ID, useValue: "de-CH"},
+    provideHttpClient(withXhr(), withInterceptorsFromDi()),
   ],
   bootstrap: [AppComponent],
 })
 export class AppModule {
-
   constructor() {
-    const masterLayoutConfig = inject(ObMasterLayoutConfig)
+    const masterLayoutConfig = inject(ObMasterLayoutConfig);
     masterLayoutConfig.homePageRoute = "/oblique-column-layout";
-  };
+  }
 }
