a87f6fef95549dd5af6325a057e53f1d61e550be
Nina Egger <nina.egger@bit.admin.ch>
2026-09-10 10:27:57 +0200

    feat(oblique-bug-report-template-new): update to oblique@16.0.0-rc.1

--- oblique-bug-report-template-new/src/app/app.component.ts
+++ oblique-bug-report-template-new/src/app/app.component.ts
@@ -1,23 +1,24 @@
-import { Component } from '@angular/core';
-import { ObINavigationLink } from '@oblique/oblique';
+import { Component, ChangeDetectionStrategy } from "@angular/core";
+import { ObINavigationLink } from "@oblique/oblique";
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    standalone: false
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  changeDetection: ChangeDetectionStrategy.Eager,
+  standalone: false,
 })
 export class AppComponent {
   topNavigation: ObINavigationLink[] = [
-    { url: 'oblique-bug-report-template-new', label: 'Home' },
+    { url: "oblique-bug-report-template-new", label: "Home" },
     {
       children: [
         {
-          label: 'Sub page',
-          url: 'sub1',
+          label: "Sub page",
+          url: "sub1",
         },
       ],
-      label: 'With sub nav',
-      url: 'menu',
+      label: "With sub nav",
+      url: "menu",
     },
   ];
 }
