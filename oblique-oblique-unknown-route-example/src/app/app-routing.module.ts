eb7a9f018b7d2d943348efa2fdcb59938d2b635b
Nina Egger <nina.egger@bit.admin.ch>
2026-09-11 10:23:00 +0200

    feat(oblique-oblique-unknown-route-example): update to oblique@16.0.0-rc.1

--- oblique-oblique-unknown-route-example/src/app/app-routing.module.ts
+++ oblique-oblique-unknown-route-example/src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { HomeComponent } from "./home/home.component";
 
 const routes: Routes = [
-  { path: '', redirectTo: 'oblique-unknown-route-example', pathMatch: 'full' },
-  { path: 'oblique-unknown-route-example', component: HomeComponent },
-  { path: '**', redirectTo: 'unknown-route' },
+  { path: "", redirectTo: "oblique-unknown-route-example", pathMatch: "full" },
+  { path: "oblique-unknown-route-example", component: HomeComponent },
+  { path: "**", redirectTo: "unknown-route" },
 ];
 
 @NgModule({
