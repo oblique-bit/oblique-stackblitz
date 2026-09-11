93820d4e70f59d65d67733beeb2ceb678dffe521
Nina Egger <nina.egger@bit.admin.ch>
2026-09-11 11:47:39 +0200

    feat(oblique-translations): update to oblique@16.0.0-rc.1

--- oblique-translations/src/app/app.component.ts
+++ oblique-translations/src/app/app.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
-import { ObINavigationLink } from '@oblique/oblique';
+import { Component, ChangeDetectionStrategy } from "@angular/core";
+import { ObINavigationLink } from "@oblique/oblique";
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    standalone: false
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  changeDetection: ChangeDetectionStrategy.Eager,
+  standalone: false,
 })
 export class AppComponent {}
--- oblique-translations/src/app/app.module.ts
+++ oblique-translations/src/app/app.module.ts
@@ -1,23 +1,27 @@
-import {inject, LOCALE_ID, NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
+import {inject, LOCALE_ID, NgModule} from "@angular/core";
+import {BrowserModule} from "@angular/platform-browser";
 
-import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './app.component';
+import {AppRoutingModule} from "./app-routing.module";
+import {AppComponent} from "./app.component";
 import {
   ObMasterLayoutModule,
   ObMasterLayoutConfig,
   ObTranslateParamsModule,
-  provideObliqueConfiguration
-} from '@oblique/oblique';
-import {registerLocaleData} from '@angular/common';
-import localeDECH from '@angular/common/locales/de-CH';
-import localeFRCH from '@angular/common/locales/fr-CH';
-import localeITCH from '@angular/common/locales/it-CH';
-import localeENCH from '@angular/common/locales/en-CH';
-import localeES from '@angular/common/locales/es';
-import {TranslateModule} from '@ngx-translate/core';
-import {TranslationsComponent} from './translations/translations.component';
-import {provideHttpClient, withInterceptorsFromDi} from '@angular/common/http';
+  provideObliqueConfiguration,
+} from "@oblique/oblique";
+import {registerLocaleData} from "@angular/common";
+import localeDECH from "@angular/common/locales/de-CH";
+import localeFRCH from "@angular/common/locales/fr-CH";
+import localeITCH from "@angular/common/locales/it-CH";
+import localeENCH from "@angular/common/locales/en-CH";
+import localeES from "@angular/common/locales/es";
+import {TranslationsComponent} from "./translations/translations.component";
+import {
+  provideHttpClient,
+  withInterceptorsFromDi,
+  withXhr,
+} from "@angular/common/http";
+import {TranslatePipe} from '@ngx-translate/core';
 
 registerLocaleData(localeDECH);
 registerLocaleData(localeENCH);
@@ -31,31 +35,30 @@ registerLocaleData(localeITCH);
     BrowserModule,
     AppRoutingModule,
     ObMasterLayoutModule,
-    
+    TranslatePipe,
     ObTranslateParamsModule,
-    TranslateModule,
   ],
   providers: [
-    provideObliqueConfiguration(
-      {
-        accessibilityStatement:
-        {
-          createdOn: new Date('2025-09-26'),
-          conformity: 'none',
-          applicationName: "Oblique Translations example",
-          applicationOperator: 'Federal Office of Information Technology, Systems and Telecommunication FOITT<br>Meielen Campus<br>Eichenweg 3<br>CH-3003 Bern',
-          contact: [{email: 'oblique@bit.admin.ch'}],
-        },
-      }),
-    {provide: LOCALE_ID, useValue: 'de-CH'},
-    provideHttpClient(withInterceptorsFromDi())
+    provideObliqueConfiguration({
+      accessibilityStatement: {
+        createdOn: new Date("2025-09-26"),
+        conformity: "none",
+        applicationName: "Oblique Translations example",
+        applicationOperator:
+          "Federal Office of Information Technology, Systems and Telecommunication FOITT<br>Meielen Campus<br>Eichenweg 3<br>CH-3003 Bern",
+        contact: [{email: "oblique@bit.admin.ch"}],
+      },
+      translate: {
+        locales: {
+          locales: ['de-CH', 'en-CH', 'es', 'fr-CH', 'it-CH'],
+          disabled: false,
+          defaultLanguage: 'de'
+        }
+      }
+    }),
+    {provide: LOCALE_ID, useValue: "de-CH"},
+    provideHttpClient(withXhr(), withInterceptorsFromDi()),
   ],
   bootstrap: [AppComponent],
 })
-export class AppModule {
-  private readonly config = inject(ObMasterLayoutConfig);
-
-  constructor() {
-    this.config.locale.locales = ['de-CH', 'en-CH', 'es', 'fr-CH', 'it-CH'];
-  }
-}
+export class AppModule {}
