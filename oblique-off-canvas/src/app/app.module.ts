673f5f820dafca992d318dc493615571eb92cc9c
Nina Egger <nina.egger@bit.admin.ch>
2026-09-11 11:25:25 +0200

    feat(oblique-off-canvas): update to oblique@16.0.0-rc.1

--- oblique-off-canvas/src/app/app.module.ts
+++ oblique-off-canvas/src/app/app.module.ts
@@ -1,45 +1,45 @@
-import {LOCALE_ID, NgModule} from '@angular/core';
-import {RouterModule} from '@angular/router';
-import {BrowserModule} from '@angular/platform-browser';
-import {AppComponent} from './app.component';
+import {LOCALE_ID, NgModule} from "@angular/core";
+import {RouterModule} from "@angular/router";
+import {BrowserModule} from "@angular/platform-browser";
+import {AppComponent} from "./app.component";
 import {
   ObMasterLayoutModule,
   ObMasterLayoutConfig,
   ObOffCanvasModule,
-  provideObliqueConfiguration
-} from '@oblique/oblique';
-import {OffCanvasContainerComponent} from './off-canvas/off-canvas-container.component';
-import {TranslateModule} from '@ngx-translate/core';
-import {provideHttpClient, withInterceptorsFromDi} from '@angular/common/http';
+  provideObliqueConfiguration,
+} from "@oblique/oblique";
+import {OffCanvasContainerComponent} from "./off-canvas/off-canvas-container.component";
+import {
+  provideHttpClient,
+  withInterceptorsFromDi,
+  withXhr,
+} from "@angular/common/http";
 
 @NgModule({
   declarations: [AppComponent, OffCanvasContainerComponent],
   imports: [
-    
     BrowserModule,
     ObMasterLayoutModule,
     ObOffCanvasModule,
-    TranslateModule,
     RouterModule.forRoot([
-      {path: '', component: OffCanvasContainerComponent},
+      {path: "", component: OffCanvasContainerComponent},
     ]),
   ],
   providers: [
-    provideObliqueConfiguration(
-      {
-        accessibilityStatement:
-        {
-          createdOn: new Date('2025-09-26'),
-          conformity: 'none',
-          applicationName: "Oblique Off Canvas example",
-          applicationOperator: 'Federal Office of Information Technology, Systems and Telecommunication FOITT<br>Meielen Campus<br>Eichenweg 3<br>CH-3003 Bern',
-          contact: [{email: 'oblique@bit.admin.ch'}],
-        },
-      }),
-    {provide: LOCALE_ID, useValue: 'de-CH'},
-    provideHttpClient(withInterceptorsFromDi())
+    provideObliqueConfiguration({
+      accessibilityStatement: {
+        createdOn: new Date("2025-09-26"),
+        conformity: "none",
+        applicationName: "Oblique Off Canvas example",
+        applicationOperator:
+          "Federal Office of Information Technology, Systems and Telecommunication FOITT<br>Meielen Campus<br>Eichenweg 3<br>CH-3003 Bern",
+        contact: [{email: "oblique@bit.admin.ch"}],
+      },
+    }),
+    {provide: LOCALE_ID, useValue: "de-CH"},
+    provideHttpClient(withXhr(), withInterceptorsFromDi()),
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule {
   constructor(config: ObMasterLayoutConfig) {
