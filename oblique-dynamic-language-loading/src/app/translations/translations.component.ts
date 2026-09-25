77921b4cf8e68e90ad0beb00af3ed99eec1e4c6a
Nina Egger <nina.egger@bit.admin.ch>
2026-09-25 07:38:15 +0200

    feat(oblique-dynamic-language-loading): update to oblique@16.0.0

--- oblique-dynamic-language-loading/src/app/translations/translations.component.ts
+++ oblique-dynamic-language-loading/src/app/translations/translations.component.ts
@@ -1,5 +1,5 @@
-import {Component, inject} from '@angular/core';
-import {TranslateService} from "@ngx-translate/core";
+import { Component, inject, ChangeDetectionStrategy } from "@angular/core";
+import { TranslateService } from "@ngx-translate/core";
 
 type DynamicLocales = {
   locales: string[];
@@ -9,11 +9,12 @@ type DynamicLocales = {
 };
 
 @Component({
-  selector: 'app-translations',
-  templateUrl: './translations.component.html',
-  styleUrls: ['./translations.component.scss'],
-  standalone: false
+  selector: "app-translations",
+  templateUrl: "./translations.component.html",
+  styleUrls: ["./translations.component.scss"],
+  changeDetection: ChangeDetectionStrategy.Eager,
+  standalone: false,
 })
 export class TranslationsComponent {
-  currentLang = inject(TranslateService).getCurrentLang()
+  currentLang = inject(TranslateService).getCurrentLang();
 }
