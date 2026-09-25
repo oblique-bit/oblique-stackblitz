bfa464c4a060bb4a63fe3d4254c03d98369e7941
Nina Egger <nina.egger@bit.admin.ch>
2026-09-25 07:49:11 +0200

    feat(oblique-off-canvas): update to oblique@16.0.0

--- oblique-off-canvas/src/app/off-canvas/off-canvas-container.component.ts
+++ oblique-off-canvas/src/app/off-canvas/off-canvas-container.component.ts
@@ -1,16 +1,17 @@
-import { Component, inject } from '@angular/core';
-import { AsyncPipe } from '@angular/common';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
+import { Component, inject, ChangeDetectionStrategy } from "@angular/core";
+import { AsyncPipe } from "@angular/common";
+import { MatButtonModule } from "@angular/material/button";
+import { MatIconModule } from "@angular/material/icon";
 import {
   ObButtonModule,
   ObOffCanvasToggleDirective,
   ObOffCanvasService,
-} from '@oblique/oblique';
+} from "@oblique/oblique";
 
 @Component({
-    selector: 'app-off-canvas-container',
-    templateUrl: './off-canvas-container.component.html',
-    standalone: false
+  selector: "app-off-canvas-container",
+  templateUrl: "./off-canvas-container.component.html",
+  changeDetection: ChangeDetectionStrategy.Eager,
+  standalone: false,
 })
 export class OffCanvasContainerComponent {}
