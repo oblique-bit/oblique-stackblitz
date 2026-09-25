08582c21a4c24988c06b519779af5c4a591560d0
Nina Egger <nina.egger@bit.admin.ch>
2026-09-25 07:35:25 +0200

    feat(oblique-column-layout): update to oblique@16.0.0

--- oblique-column-layout/src/app/app-routing.module.ts
+++ oblique-column-layout/src/app/app-routing.module.ts
@@ -1,10 +1,10 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { HomeComponent } from "./home/home.component";
 
 const routes: Routes = [
-  { path: '', redirectTo: 'oblique-column-layout', pathMatch: 'full' },
-  { path: 'oblique-column-layout', component: HomeComponent }
+  { path: "", redirectTo: "oblique-column-layout", pathMatch: "full" },
+  { path: "oblique-column-layout", component: HomeComponent },
 ];
 
 @NgModule({
--- oblique-column-layout/src/app/home/home.component.ts
+++ oblique-column-layout/src/app/home/home.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from "@angular/core";
 
 @Component({
-    selector: 'app-home',
-    templateUrl: './home.component.html',
-    standalone: false
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  changeDetection: ChangeDetectionStrategy.Eager,
+  standalone: false,
 })
 export class HomeComponent {}
