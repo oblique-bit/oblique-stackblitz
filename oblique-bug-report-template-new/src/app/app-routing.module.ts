27925225e94cd89dcefbf539a2c13ce688b7999a
Nina Egger <nina.egger@bit.admin.ch>
2026-09-25 07:33:20 +0200

    feat(oblique-bug-report-template-new): update to oblique@16.0.0

--- oblique-bug-report-template-new/src/app/app-routing.module.ts
+++ oblique-bug-report-template-new/src/app/app-routing.module.ts
@@ -1,12 +1,16 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { SubPageComponent } from './sub-page/sub-page.component';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { HomeComponent } from "./home/home.component";
+import { SubPageComponent } from "./sub-page/sub-page.component";
 
 const routes: Routes = [
-  { path: '', redirectTo: 'oblique-bug-report-template-new', pathMatch: 'full' },
-  { path: 'oblique-bug-report-template-new', component: HomeComponent },
-  { path: 'menu/sub1', component: SubPageComponent },
+  {
+    path: "",
+    redirectTo: "oblique-bug-report-template-new",
+    pathMatch: "full",
+  },
+  { path: "oblique-bug-report-template-new", component: HomeComponent },
+  { path: "menu/sub1", component: SubPageComponent },
 ];
 
 @NgModule({
