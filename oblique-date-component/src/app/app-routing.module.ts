64f5618df01141cb46c48878116b32533db52db8
Nina Egger <nina.egger@bit.admin.ch>
2026-09-25 07:36:37 +0200

    feat(oblique-date-component): update to oblique@16.0.0

--- oblique-date-component/src/app/app-routing.module.ts
+++ oblique-date-component/src/app/app-routing.module.ts
@@ -1,10 +1,10 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { HomeComponent } from "./home/home.component";
 
 const routes: Routes = [
-  { path: '', redirectTo: 'oblique-date-component', pathMatch: 'full' },
-  { path: 'oblique-date-component', component: HomeComponent },
+  { path: "", redirectTo: "oblique-date-component", pathMatch: "full" },
+  { path: "oblique-date-component", component: HomeComponent },
 ];
 
 @NgModule({
--- oblique-date-component/src/app/home/home.component.ts
+++ oblique-date-component/src/app/home/home.component.ts
@@ -1,10 +1,11 @@
-import {Component} from '@angular/core';
+import { Component, ChangeDetectionStrategy } from "@angular/core";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  standalone: false
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  changeDetection: ChangeDetectionStrategy.Eager,
+  standalone: false,
 })
 export class HomeComponent {
-  exampleDate = new Date('2025-05-12');
+  exampleDate = new Date("2025-05-12");
 }
