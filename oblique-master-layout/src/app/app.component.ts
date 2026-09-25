fbfc43fe9fc6c7d2917422e91810d5074f332911
Nina Egger <nina.egger@bit.admin.ch>
2026-09-25 07:41:10 +0200

    feat(oblique-master-layout): update to oblique@16.0.0

--- oblique-master-layout/src/app/app.component.ts
+++ oblique-master-layout/src/app/app.component.ts
@@ -1,27 +1,28 @@
-import { Component } from '@angular/core';
-import { ObINavigationLink } from '@oblique/oblique';
+import { Component, ChangeDetectionStrategy } from "@angular/core";
+import { ObINavigationLink } from "@oblique/oblique";
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    standalone: false
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  changeDetection: ChangeDetectionStrategy.Eager,
+  standalone: false,
 })
 export class AppComponent {
   topNavigation: ObINavigationLink[] = [
-    { url: 'oblique-master-layout', label: 'Home' },
+    { url: "oblique-master-layout", label: "Home" },
     {
       children: [
         {
-          label: 'Sub page',
-          url: 'sub1',
+          label: "Sub page",
+          url: "sub1",
         },
       ],
-      label: 'With sub nav',
-      url: 'menu',
+      label: "With sub nav",
+      url: "menu",
     },
     {
-      label: 'Removable item',
-      url: 'menu',
+      label: "Removable item",
+      url: "menu",
       removable: true,
     },
   ];
