49987ab808a9819a6e6046ff82fa2f5b2d034a1e
Nina Egger <nina.egger@bit.admin.ch>
2026-09-25 12:24:34 +0200

    feat(oblique-master-layout): add linting
    OUI-4567

--- oblique-master-layout/src/app/app.module.ts
+++ oblique-master-layout/src/app/app.module.ts
@@ -1,14 +1,14 @@
-import { LOCALE_ID, NgModule } from "@angular/core";
-import { BrowserModule } from "@angular/platform-browser";
+import {LOCALE_ID, NgModule, inject} from "@angular/core";
+import {BrowserModule} from "@angular/platform-browser";
 
-import { AppRoutingModule } from "./app-routing.module";
-import { AppComponent } from "./app.component";
+import {AppRoutingModule} from "./app-routing.module";
+import {AppComponent} from "./app.component";
 import {
+  ObMasterLayoutConfig,
   ObMasterLayoutModule,
   provideObliqueConfiguration,
-  ObMasterLayoutConfig,
 } from "@oblique/oblique";
-import { registerLocaleData } from "@angular/common";
+import {registerLocaleData} from "@angular/common";
 import localeDECH from "@angular/common/locales/de-CH";
 import localeFRCH from "@angular/common/locales/fr-CH";
 import localeITCH from "@angular/common/locales/it-CH";
@@ -17,17 +17,16 @@ import {
   withInterceptorsFromDi,
   withXhr,
 } from "@angular/common/http";
-import { HomeComponent } from "./home/home.component";
-import { SubPageComponent } from "./sub-page/sub-page.component";
-import { inject } from "@angular/core";
+import {HomeComponent} from "./home/home.component";
+import {SubPageComponent} from "./sub-page/sub-page.component";
 
 registerLocaleData(localeDECH);
 registerLocaleData(localeFRCH);
 registerLocaleData(localeITCH);
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, SubPageComponent],
   imports: [BrowserModule, AppRoutingModule, ObMasterLayoutModule],
+  declarations: [AppComponent, HomeComponent, SubPageComponent],
   providers: [
     provideObliqueConfiguration({
       accessibilityStatement: {
@@ -36,13 +35,13 @@ registerLocaleData(localeITCH);
         applicationName: "Oblique Master-Layout example",
         applicationOperator:
           "Federal Office of Information Technology, Systems and Telecommunication FOITT<br>Meielen Campus<br>Eichenweg 3<br>CH-3003 Bern",
-        contact: [{ email: "oblique@bit.admin.ch" }],
+        contact: [{email: "oblique@bit.admin.ch"}],
       },
     }),
-    { provide: LOCALE_ID, useValue: "de-CH" },
+    {provide: LOCALE_ID, useValue: "de-CH"},
     provideHttpClient(withXhr(), withInterceptorsFromDi()),
   ],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent]
 })
 export class AppModule {
   constructor() {
